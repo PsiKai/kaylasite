503f29076c511e4397c244d9bb2d15e1f1490aa0
David Irvin <davidirvin47@gmail.com>
2023-11-19 14:48:23 -0700

    Create collapse animation for alert toasts

--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -21,19 +21,19 @@ export default function Alerts() {
   return (
     <div className="alerts-container">
       {alerts.map(alert => (
-        <div
-          key={alert.id}
-          data-alertid={alert.id}
-          className="alert-toast colored"
-          role="alert"
-          style={{ "--alert-type": `var(--${alert.type || "primary"})` }}
-        >
-          <i className={`fa fa-${icons(alert.type)}`} aria-hidden="true"></i>
+        <div key={alert.id} data-alertid={alert.id}>
+          <div
+            className="alert-toast colored"
+            role="alert"
+            style={{ "--alert-type": `var(--${alert.type || "primary"})` }}
+          >
+            <i className={`fa fa-${icons(alert.type)}`} aria-hidden="true"></i>
 
-          <span className="alert-message">{alert.message}</span>
-          <button className="alert-dismiss" onClick={() => dismissAlert(alert.id)}>
-            <i className="fa fa-times-circle"></i>
-          </button>
+            <span className="alert-message">{alert.message}</span>
+            <button className="alert-dismiss" onClick={() => dismissAlert(alert.id)}>
+              <i className="fa fa-times-circle"></i>
+            </button>
+          </div>
         </div>
       ))}
     </div>
--- src/hooks/useAlerts.js
+++ src/hooks/useAlerts.js
@@ -10,22 +10,37 @@ export default function useAlerts(alertDuration = 7_000) {
     dispatch({ type: "NEW_ALERT", payload: { id, message, type } })
 
     setTimeout(() => {
-      dismissAlert(id)
+      requestAnimationFrame(() => dismissAlert(id))
     }, alertDuration)
   }
 
   function dismissAlert(id) {
     const alert = document.querySelector(`[data-alertid="${id}"]`)
-    const slide = new KeyframeEffect(alert, [{ translate: "200% 0" }], {
+    if (!alert) return
+
+    const { height } = alert.getBoundingClientRect()
+    const slideFrames = new KeyframeEffect(alert, [{ translate: "200% 0" }], {
       duration: 200,
       easing: "ease",
-      direction: "normal",
       fill: "forwards",
     })
 
-    const animation = new Animation(slide, document.timeline)
-    animation.play()
-    animation.onfinish = () => {
+    const collapseFrames = new KeyframeEffect(
+      alert,
+      [{ maxHeight: `${height}px` }, { maxHeight: "0" }],
+      {
+        duration: 100,
+        easing: "ease-in",
+        fill: "forwards",
+      }
+    )
+    const slideAnimation = new Animation(slideFrames, document.timeline)
+    const collapseAnimation = new Animation(collapseFrames, document.timeline)
+    slideAnimation.play()
+    slideAnimation.onfinish = () => {
+      collapseAnimation.play()
+    }
+    collapseAnimation.onfinish = () => {
       dispatch({ type: "REMOVE_ALERT", payload: { id } })
     }
   }
